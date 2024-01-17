e7b5c73d816608bd045a8998bb211cd9c2bb1353
Scxar <oscar28lof@gmail.com>
2024-01-17 10:59:02 +0100

    la till funktion för kön

--- Grupp_3/Operation.cs
+++ Grupp_3/Operation.cs
@@ -25,10 +25,17 @@ namespace Grupp_3
         {
 
         }
-        public string Gender(long personnummer)
-        {
 
+        public string Gender(string personnummer)
+        {    
+            // "Oscar", säga till om person nummret tillhör man eller kvinna ! 
+            int GenderNr = int.Parse(personnummer.Substring(8, 1));
+    
+            if (GenderNr % 2 == 0)
+            {
+                return "Kvinna";
+            }
+            return "Man";
         }
-
     }
 }
